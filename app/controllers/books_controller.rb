abd62c759a693acaf5a5e3f0d11123a32b191d3c
EC2 Default User <ec2-user@ip-172-31-45-231.ec2.internal>
2022-03-24 05:51:19 +0000

    data switch

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+      if @book.user == current_user
+            render "edit"
+      else
+            redirect_to books_path
+      end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+     if @user == current_user
+            render "edit"
+     else
+            redirect_to user_path
+     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
