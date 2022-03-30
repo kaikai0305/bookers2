a82ab749b0ff113d8ffaf2f5ddc6e7deac973cf5
kaikai0305 <kaikai030552@gmail.com>
2022-03-30 12:36:17 +0000

    エラーメッセージサクセスメッセージ表示完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,13 @@ class ApplicationController < ActionController::Base
 
 
  def after_sign_in_path_for(resource)
+   flash[:notice] = "Welcome! You have signed up successfully."
     user_path(resource)
  end
 
 
  def after_sign_out_path_for(resource)
+   flash[:notice] = "Signed out successfully."
     root_path
  end
 
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
     @user = current_user
   end
 
@@ -14,17 +15,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-      if @book.user == current_user
-            render "edit"
-      else
-            redirect_to books_path
-      end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "You have updated book successfully."
+     redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
   end
 
   def destroy
@@ -37,9 +37,10 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to books_path
     else
-      render :new
+      render :index
     end
   end
 
