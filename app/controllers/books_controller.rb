f792f0a3751a65e173cd371c92a7889c80734bf8
kaikai0305 <kaikai030552@gmail.com>
2022-03-30 08:35:30 +0000

    レイアウト変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts << "3409f6a0c32549e78eb736f472a0ad3e.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "564314732fdb4a508cc2bca510a355fb.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
    config.active_job.queue_adapter = :inline
